591d4ea552298cf8e5f6ad7d11b356097e9ab900
Benjamin Consolvo <bconsolvo@users.noreply.github.com>
2018-08-08 12:39:19 -0500

    Add files via upload

---
+++ SQLQuery1_SalaryExample.sql
@@ -0,0 +1,35 @@
+
+/* Create a new database 
+CREATE DATABASE salary_info */
+
+/* Go into the database just created 
+USE salary_info
+
+Create a new table for the salaries of employees 
+CREATE TABLE salary
+(
+ Name varchar(50),
+ USDAnnualSalary money
+);
+
+*/ 
+/* Inserting names and salary values into the newly created table */
+INSERT INTO salary
+(Name, USDAnnualSalary)
+VALUES
+('Ben',60000);
+
+INSERT INTO salary
+(Name, USDAnnualSalary)
+VALUES
+ ('Shay', 80000);
+
+INSERT INTO salary
+(Name, USDAnnualSalary)
+VALUES
+ ('Mike', 50000);
+ 
+ SELECT *
+ From salary;
+
+ 
